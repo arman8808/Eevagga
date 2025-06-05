b66228c04ec49849790c9d8eb7b3959760c0efd7
arman8808 <armanal3066@gmail.com>
2025-06-05 20:22:46 +0530

    added images

--- src/components/Slider/SliderNew.jsx
+++ src/components/Slider/SliderNew.jsx
@@ -11,6 +11,12 @@ import BannerImg7 from "../../assets/banner_new4.webp";
 import BannerImg8 from "../../assets/banner_new1.webp";
 import BannerImg9 from "../../assets/banner_new2.webp";
 import BannerImg10 from "../../assets/banner_new3.webp";
+import BannerImg11 from "../../assets/banner 1.jpeg";
+import BannerImg12 from "../../assets/banner 2.jpg";
+import BannerImg13 from "../../assets/banner 3.jpeg";
+import BannerImg14 from "../../assets/banner 4.jpeg";
+import BannerImg15 from "../../assets/banner 5.jpg";
+import BannerImg16 from "../../assets/banner 6.jpg";
 import BannerNew from "../Banner/BannerNew";
 
 function SliderNew({}) {
@@ -25,6 +31,12 @@ function SliderNew({}) {
   };
   const img = [
  
+    BannerImg16,
+    BannerImg15,
+    BannerImg14,
+    BannerImg13,
+    BannerImg12,
+    BannerImg11,
     BannerImg10,
     BannerImg9,
     BannerImg8,
